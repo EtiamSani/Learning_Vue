a6f7ea72325d2a58f571eac75a7270952ae9ffff
EtiamSani <etiam.sani@laposte.net>
2023-11-15 16:13:33 +0100

    Vue task tracker

--- learning-vue/src/App.vue
+++ learning-vue/src/App.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="container">
-    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
     <router-view :showAddTask="showAddTask"></router-view>
-
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Header,
-
+    Footer,
   },
   data() {
     return {
       showAddTask: false,
-    };
+    }
   },
   methods: {
     toggleAddTask() {
-      this.showAddTask = !this.showAddTask;
+      this.showAddTask = !this.showAddTask
     },
   },
-};
+}
 </script>
 
-<style scoped>
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 
 * {
--- learning-vue/src/components/Header.vue
+++ learning-vue/src/components/Header.vue
@@ -1,42 +1,44 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-        <Button v-show="homePage" @btn-click="toggleAddTask" :text="showAddTask ? 'Close' : 'Add Task'"
-            :color="showAddTask ? 'red' : 'green'" />
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
 </template>
-  
+
 <script>
-import Button from './Button.vue';
+import Button from './Button'
 
 export default {
-    name: 'Header',
-    props: {
-        title: String,
-        showAddTask: Boolean,
+  name: 'Header',
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
     },
-    components: {
-        Button,
-    },
-    computed: {
-        homePage() {
-            // return this.$route.path === '/';
-        },
-    },
-    methods: {
-        toggleAddTask() {
-            this.$emit('toggle-add-task');
-        },
-    },
-};
+  },
+}
 </script>
-  
+
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
-  
